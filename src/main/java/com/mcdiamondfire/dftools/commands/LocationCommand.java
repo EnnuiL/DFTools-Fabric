98328831bac16375997019f93a135bc013535a6f
joaoh1 <joaoh1@users.noreply.github.com>
2019-09-07 16:51:25 -0300

    We are alive, but on roadblock DFTools Fabric 0.3.2 will probably be done instead of dev 0.4.0, anyway: Updated gradle Made the location command drastically smaller

--- src/main/java/com/mcdiamondfire/dftools/commands/LocationCommand.java
+++ src/main/java/com/mcdiamondfire/dftools/commands/LocationCommand.java
@@ -49,7 +49,7 @@ public class LocationCommand {
 			MessageUtils.errorMessage("You need to be in build mode or dev mode to do this!");
 			return 1;
         }
-
+        
         ItemStack itemStack = minecraft.player.getMainHandStack();
         
         // Checks if item stack is not air.
@@ -57,70 +57,78 @@ public class LocationCommand {
             MessageUtils.errorMessage("Invalid item!");
             return 1;
         }
-
+        
         // Checks if item has an NBT tag.
         if (!itemStack.hasTag()) {
             MessageUtils.errorMessage("Invalid item!");
             return 1;
         }
-
+        
         // Checks if item has the correct name.
         if (!itemStack.getName().getString().equals("§aLocation")) {
             MessageUtils.errorMessage("Invalid item!");
             return 1;
         }
-
+        
         // Checks if item has HideFlags and if it's the correct one.
         if (!itemStack.getTag().containsKey("HideFlags") || !(itemStack.getTag().getInt("HideFlags") == 63)) {
 			MessageUtils.errorMessage("Invalid item!");
 			return 1;
 		}
-
+        
         // TODO: Remove the ViaVersion hack when DF gets updated to 1.14.4.
         ListTag itemLore = itemStack.getSubTag("display").getList("Lore", 8);
+        
+        // Dramatically shrinks the lines
+        String loreLineBeginning = "{\"extra\":[{\"color\":\"gray\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"";
+        String loreLineEnd = ": \"},{\"color\":\"white\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"";
         
         // Sets the X coordinate if specified.
         if (paramCount >= 1) {
             // Gets the parameter.
             double shiftX = DoubleArgumentType.getDouble(context, "x");
             // Extracts the coordinates from the location's lore.
-            double originalX = Double.parseDouble(itemLore.getString(0).toString().replace("{\"extra\":[{\"color\":\"gray\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"X: \"},{\"color\":\"white\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"", "").replace("\"}],\"text\":\"\"}", ""));
+            double originalX = Double.parseDouble(itemLore.getString(0).toString().replace(loreLineBeginning + "X" + loreLineEnd, "").replace("\"}],\"text\":\"\"}", ""));
             // Set the location coordinates.
-            itemLore.setTag(0, new StringTag("{\"extra\":[{\"color\":\"gray\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"X: \"},{\"color\":\"white\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"" + (MathUtils.round(originalX + shiftX, 2)) + "\"}],\"text\":\"\"}"));
+            itemLore.setTag(0, new StringTag(loreLineBeginning + "X" + loreLineEnd + (MathUtils.round(originalX + shiftX, 2)) + "\"}],\"text\":\"\"}"));
         }
-
+        
         // Sets the Y coordinate if specified.
         if (paramCount >= 2) {
             double shiftY = DoubleArgumentType.getDouble(context, "y");
-            double originalY = Double.parseDouble(itemLore.getString(1).toString().replace("{\"extra\":[{\"color\":\"gray\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"Y: \"},{\"color\":\"white\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"", "").replace("\"}],\"text\":\"\"}", ""));
-            itemLore.setTag(1, new StringTag("{\"extra\":[{\"color\":\"gray\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"Y: \"},{\"color\":\"white\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"" + (MathUtils.round(originalY + shiftY, 2)) + "\"}],\"text\":\"\"}"));
+            double originalY = Double.parseDouble(itemLore.getString(1).toString().replace(loreLineBeginning + "Y" + loreLineEnd, "").replace("\"}],\"text\":\"\"}", ""));
+            itemLore.setTag(1, new StringTag(loreLineBeginning + "Y" + loreLineEnd + (MathUtils.round(originalY + shiftY, 2)) + "\"}],\"text\":\"\"}"));
         }
-
+        
         // Sets the Z coordinate if specified. 
         if (paramCount >= 3) {
             double shiftZ = DoubleArgumentType.getDouble(context, "z");
-            double originalZ = Double.parseDouble(itemLore.getString(2).toString().replace("{\"extra\":[{\"color\":\"gray\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"Z: \"},{\"color\":\"white\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"", "").replace("\"}],\"text\":\"\"}", ""));
-            itemLore.setTag(2, new StringTag("{\"extra\":[{\"color\":\"gray\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"Z: \"},{\"color\":\"white\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"" + (MathUtils.round(originalZ + shiftZ, 2)) + "\"}],\"text\":\"\"}"));
+            double originalZ = Double.parseDouble(itemLore.getString(2).toString().replace(loreLineBeginning + "Z" + loreLineEnd, "").replace("\"}],\"text\":\"\"}", ""));
+            itemLore.setTag(2, new StringTag(loreLineBeginning + "Z" + loreLineEnd + (MathUtils.round(originalZ + shiftZ, 2)) + "\"}],\"text\":\"\"}"));
         }
-
+        
         // Sets the pitch if specified.
         if (paramCount >= 4)  {
             double shiftPitch = DoubleArgumentType.getDouble(context, "pitch");
             double originalPitch = 0;
-            originalPitch = Double.parseDouble(itemLore.getString(3).toString().replace("{\"extra\":[{\"color\":\"gray\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"p: \"},{\"color\":\"white\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"", "").replace("\"}],\"text\":\"\"}", ""));
-            itemLore.setTag(3, new StringTag("{\"extra\":[{\"color\":\"gray\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"p: \"},{\"color\":\"white\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"" + (MathUtils.round(MathUtils.clamp((originalPitch + shiftPitch), -90, 90), 2)) + "\"}],\"text\":\"\"}"));
+            originalPitch = Double.parseDouble(itemLore.getString(3).toString().replace(loreLineBeginning + "p" + loreLineEnd, "").replace("\"}],\"text\":\"\"}", ""));
+            itemLore.setTag(3, new StringTag(loreLineBeginning + "p" + loreLineEnd + (MathUtils.roundAndClamp((originalPitch + shiftPitch), -90, 90, 2)) + "\"}],\"text\":\"\"}"));
         }
         
         // Sets the yaw if specified.
         if (paramCount >= 5) {
             double shiftYaw = DoubleArgumentType.getDouble(context, "yaw");
             double originalYaw = 0;
-            originalYaw = Double.parseDouble(itemLore.getString(4).toString().replace("{\"extra\":[{\"color\":\"gray\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"y: \"},{\"color\":\"white\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"", "").replace("\"}],\"text\":\"\"}", ""));
-            itemLore.setTag(4, new StringTag("{\"extra\":[{\"color\":\"gray\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"y: \"},{\"color\":\"white\",\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"text\":\"" + (MathUtils.round(MathUtils.clamp((originalYaw + shiftYaw), -180, 180), 2)) + "\"}],\"text\":\"\"}"));
+            originalYaw = Double.parseDouble(itemLore.getString(4).toString().replace(loreLineBeginning + "y" + loreLineEnd, "").replace("\"}],\"text\":\"\"}", ""));
+            itemLore.setTag(4, new StringTag(loreLineBeginning + "y" + loreLineEnd + (MathUtils.roundAndClamp((originalYaw + shiftYaw), -180, 180, 2)) + "\"}],\"text\":\"\"}"));
+        }
+        
+        //Sends updated item to the server.
+        if (paramCount >= 1) {
+            ItemUtils.setItemInHand(itemStack);
+            MessageUtils.infoMessage("yay!");
         }
 
-        //Sends updated item to the server.
-        ItemUtils.setItemInHand(itemStack);
         return 1;
     }
 }
--- src/main/java/com/mcdiamondfire/dftools/utils/MathUtils.java
+++ src/main/java/com/mcdiamondfire/dftools/utils/MathUtils.java
@@ -22,11 +22,16 @@ public class MathUtils {
 		return number;
 	}
 
-	public static double round(double value, int places) {
+	public static double round(double number, int places) {
 		if (places < 0) throw new IllegalArgumentException();
 	
-		BigDecimal bd = BigDecimal.valueOf(value);
+		BigDecimal bd = BigDecimal.valueOf(number);
 		bd = bd.setScale(places, RoundingMode.HALF_UP);
 		return bd.doubleValue();
+	}
+
+	public static double roundAndClamp(double number, double min, double max, int places) {
+		double clampledNumber = MathUtils.clamp(number, min, max);
+		return MathUtils.round(clampledNumber, places);
 	}
 }
