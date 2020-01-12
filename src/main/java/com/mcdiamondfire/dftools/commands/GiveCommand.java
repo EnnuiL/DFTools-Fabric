00dc156287600278523e0615c3f56490211291c3
joaoh1 <joaoh1@users.noreply.github.com>
2020-01-11 23:43:42 -0300

    Add /dfgive get

--- src/main/java/com/mcdiamondfire/dftools/commands/GiveCommand.java
+++ src/main/java/com/mcdiamondfire/dftools/commands/GiveCommand.java
@@ -11,6 +11,13 @@ import net.minecraft.command.arguments.ItemStackArgumentType;
 import io.github.cottonmc.clientcommands.*;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.registry.Registry;
 
 public class GiveCommand {
     private static final MinecraftClient minecraft = MinecraftClient.getInstance();
@@ -23,6 +30,8 @@ public class GiveCommand {
             .then(ArgumentBuilders.argument("count", IntegerArgumentType.integer(1, 64))
                 .executes(ctx -> execute(ctx, true)))
             .executes(ctx -> execute(ctx, false)))
+        .then(ArgumentBuilders.literal("get")
+            .executes(ctx -> executeGet(ctx)))
         .executes(ctx -> {
             if (!minecraft.player.isCreative()) {
                 MessageUtils.errorMessage("You need to be in build mode or dev mode to do this!");
@@ -38,6 +47,8 @@ public class GiveCommand {
             .then(ArgumentBuilders.argument("count", IntegerArgumentType.integer(1, 64))
                 .executes(ctx -> execute(ctx, true)))
             .executes(ctx -> execute(ctx, false)))
+        .then(ArgumentBuilders.literal("get")
+            .executes(ctx -> executeGet(ctx)))
         .executes(ctx -> {
             if (!minecraft.player.isCreative()) {
                 MessageUtils.errorMessage("You need to be in build mode or dev mode to do this!");
@@ -70,6 +81,43 @@ public class GiveCommand {
         
         //Sends updated item to the server.
         ItemUtils.setItemInHotbar(itemStack, false);
+        return 1;
+    }
+
+    private static int executeGet(CommandContext<CottonClientCommandSource> context) throws CommandSyntaxException {
+        ItemStack itemStack = minecraft.player.getMainHandStack();
+
+        //Checks if player is not in survival mode.
+        if (!minecraft.player.isCreative()) {
+            MessageUtils.errorMessage("You need to be in build mode or dev mode to do this!");
+            return 1;
+        }
+
+        //Checks if item stack is not air.
+		if (itemStack.isEmpty()) {
+			MessageUtils.errorMessage("Invalid item!");
+            return 1;
+		}
+
+        String itemId = Registry.ITEM.getId(itemStack.getItem()).toString();
+        String itemTag = itemStack.getOrCreateTag().toString();
+
+        if (itemTag.equals("{}")) {
+            itemTag = "";
+        }
+        
+        //Creates the click and hover events for the message.
+		Style messageStyle = new Style();
+		messageStyle.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/dftcopy text " + itemId + itemTag));
+		messageStyle.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new LiteralText("Click here to copy the /give-formatted\nitem data to your clipboard.").formatted(Formatting.BLUE)));
+		
+		//Creates the actual message text component.
+		Text messageText = new LiteralText(itemId + itemTag).formatted(Formatting.BLUE);
+		messageText.setStyle(messageStyle);
+		
+		//Sends the message.
+		MessageUtils.infoMessage("/give-formatted item data:");
+        minecraft.player.sendMessage(messageText);
         return 1;
     }
 }
