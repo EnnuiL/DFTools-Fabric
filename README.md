d8157b2a1621a39d517b54ad40e2d67d3ca128b4
joaoh1 <7559492+joaoh1@users.noreply.github.com>
2020-03-23 15:25:47 -0300

    Finally! Functional /dfgive!

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# DiamondFire Tools: Fabric Edition
-This is a port of the DiamondFire Tools mod, which was originally made for Forge, this port aims to rewrite all the features so they can take advantage of new systems, such as the Brigadier system.<br>
+<img src="./src/main/resources/assets/crid/icon.png" align="right" width="128px"/>
 
-Currently, the focus is to provide new GUIs in general, like one for /give and one that replaces the game menu.
+# Boring Template
+
+This is my personal template, it's specially made for my own needs, and therefore, it's not suited to other human beings (however, you can still use it, if you don't mind the personal tweaks). This is a fork of the [Fabric Example Mod](https://github.com/FabricMC/fabric-example-mod), which is more generic, therefore, a better template for regular human beings. This template also borrows [Blueprint's](https://github.com/FabLabsMC/Blueprint) GitHub Actions/Packages integration, most of the integration is directly (or indirectly) from there.
+
+## Usage
+
+You should be able to setup BoringTemplates for your own usage by the same way you setup the Fabric Example Mod template. The mysterious `crid` should not be questioned but it should be replaced with a real mod id. 
+
+## License
+
+This template, like the original template which this is based on, is available under the CC0 license. You are free to do whatever you want with this template. A copy of the MIT license is contained in this repository as a personal quality-of-life feature and it isn't applied on this template.
--- src/main/java/com/mcdiamondfire/dftools/screen/GiveCommandScreen.java
+++ src/main/java/com/mcdiamondfire/dftools/screen/GiveCommandScreen.java
@@ -3,7 +3,6 @@ package com.mcdiamondfire.dftools.screen;
 import com.mcdiamondfire.dftools.utils.ItemUtils;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.StringNbtReader;
@@ -14,85 +13,73 @@ import spinnery.client.BaseScreen;
 import spinnery.widget.WInterface;
 import spinnery.widget.WPanel;
 import spinnery.widget.WButton;
-import spinnery.widget.WStaticText;
 import spinnery.widget.WTextField;
 import spinnery.widget.api.Position;
-import spinnery.widget.api.Size;
 
 public class GiveCommandScreen extends BaseScreen {
 	public GiveCommandScreen() {
 		super();
-		
-		MinecraftClient minecraftClient = MinecraftClient.getInstance();
 
 		WInterface mainInterface = getInterface();
 		
-		WPanel mainPanel = mainInterface.getFactory().build(WPanel.class);
-		mainPanel.getSize().setWidth(250);
-		mainPanel.getSize().setHeight(100);
-		
+		WPanel mainPanel = new WPanel();
+		mainPanel.getSize().setWidth(250).setHeight(100);
+		//mainPanel.center();
+		mainPanel.setPosition(Position.of(mainPanel.getPosition())
+			.setX(mainInterface.getX() + mainInterface.getWidth() / 2 - mainPanel.getWidth() / 2)
+			.setY(mainInterface.getY() + mainInterface.getHeight() / 2 - mainPanel.getHeight() / 2)
+		);
 		mainPanel.setLabel("Give Item");
-		mainPanel.center();
 
 		setIsPauseScreen(true);
 		mainInterface.setBlurred(true);
 
-		//WStaticText menuLabel = new WStaticText(WPosition.of(WType.ANCHORED, 6, 6, 0, mainInterface), mainInterface, new LiteralText("Give Item"));
-		//menuLabel.setLabelShadow(true);
-
 		WTextField textBox = new WTextField();
-		textBox.getPosition().set(238, 63, 0);
-		textBox.getSize().setWidth(6).setHeight(18);
+		textBox.getPosition().setAnchor(mainPanel).set(6, 18, 0);
+		textBox.getSize().setWidth(238).setHeight(63);
 		textBox.setLabel(new LiteralText("minecraft:item_id{Tag:Here}"));
 
-		/*
-		//WButton giveButton = new WButton(WPosition.of(WType.ANCHORED, 218, 82, 0, mainInterface), WSize.of(26, 14), mainInterface);
 		WButton giveButton = new WButton();
-		giveButton.getPosition().set(218, 82, 0);
+		giveButton.getPosition().setAnchor(mainPanel).set(218, 82, 0);
 		giveButton.getSize().setWidth(26).setHeight(14);
 		giveButton.setLabel(new LiteralText("Give"));
 
-		giveButton.onMouseClicked((int)minecraft.mouse.getX(), (int)minecraft.mouse.getY(), 0) -> {};
-
-		giveButton.setOnMouseClicked(() -> {
-			if (giveButton.isLowered()) {
-				String[] itemIdArray = textBox.getText().split("[{]", 2);
-				String[] itemIdArrayWithAmount = textBox.getText().split(" (?!.* )", 2);
-				String itemIdentifier = itemIdArray[0];
-				String itemTag = "{}";
-				String amount = "1";
-				if (itemIdArray.length == 2) {
-					itemTag = "{" + itemIdArray[1];
-				}
-				if (itemIdArrayWithAmount.length == 2) {
-					if (itemTag == "{}") {
-						itemIdentifier = itemIdArrayWithAmount[0].replace(itemTag, "");
-					} else {
-						itemTag = itemIdArrayWithAmount[0].replace(itemIdentifier, "");
-					}
-					amount = itemIdArrayWithAmount[1];
-				}
-				Identifier itemStackID = new Identifier(itemIdentifier);
-				ItemStack itemStack = new ItemStack(Registry.ITEM.get(itemStackID));
-				if (itemIdArray.length == 2) {
-					try {
-						CompoundTag itemStackTag = StringNbtReader.parse(itemTag);
-						itemStack.setTag(itemStackTag);
-					} catch (CommandSyntaxException e) {
-						e.printStackTrace();
-					}
-				}
-				int itemStackAmount = Integer.parseInt(amount);
-				if (itemStackAmount >= itemStack.getMaxCount()) {
-					itemStackAmount = itemStack.getMaxCount();
-				}
-				itemStack.setCount(itemStackAmount);
-
-				ItemUtils.setItemInHotbar(itemStack, false);
+		giveButton.setOnMouseClicked((WButton w, int mouseX, int mouseY, int mouseButton) -> {
+			String[] itemIdArray = textBox.getText().split("[{]", 2);
+			String[] itemIdArrayWithAmount = textBox.getText().split(" (?!.* )", 2);
+			String itemIdentifier = itemIdArray[0];
+			String itemTag = "{}";
+			String amount = "1";
+			if (itemIdArray.length == 2) {
+				itemTag = "{" + itemIdArray[1];
 			}
-		});
-		*/
+			if (itemIdArrayWithAmount.length == 2) {
+				if (itemTag == "{}") {
+					itemIdentifier = itemIdArrayWithAmount[0].replace(itemTag, "");
+				} else {
+					itemTag = itemIdArrayWithAmount[0].replace(itemIdentifier, "");
+				}
+				amount = itemIdArrayWithAmount[1];
+			}
+			Identifier itemStackID = new Identifier(itemIdentifier);
+			ItemStack itemStack = new ItemStack(Registry.ITEM.get(itemStackID));
+			if (itemIdArray.length == 2) {
+				try {
+					CompoundTag itemStackTag = StringNbtReader.parse(itemTag);
+					itemStack.setTag(itemStackTag);
+				} catch (CommandSyntaxException e) {
+					e.printStackTrace();
+				}
+			}
+			int itemStackAmount = Integer.parseInt(amount);
+			if (itemStackAmount >= itemStack.getMaxCount()) {
+				itemStackAmount = itemStack.getMaxCount();
+			}
+			itemStack.setCount(itemStackAmount);
 
-		mainInterface.add(mainPanel, textBox);
+			ItemUtils.setItemInHotbar(itemStack, false);
+		});
+
+		mainInterface.add(giveButton, textBox, mainPanel);
 	}
 }
